56e9675dd9b9f5cd2da7c3cba0f14d01d98bdd63
Trevor Killeen <killeent@cs.washington.edu>
2015-02-04 15:04:34 -0800

    implement ECS interface

--- src/ElevatorControlService.java
+++ src/ElevatorControlService.java
@@ -1,5 +1,44 @@
+import java.util.List;
+import java.util.Set;
+
 /**
- * Created by trevor on 2/4/15.
+ * Interface for an ECS.
+ *
+ * @author Trevor Killeen (2015)
  */
 public interface ElevatorControlService {
+
+    /**
+     * Returns the status of the ECS.
+     *
+     * @return A list of triples of the form (elevatorID, current floor, target floor).
+     */
+    public List<Triple<Integer>> status();
+
+    /**
+     * Update the destination floor for specified elevator.
+     *
+     * @param elevatorID The elevator to update.
+     * @param destinationFloor destination floor.
+     */
+    public void update(int elevatorID, int destinationFloor);
+
+    /**
+     * @return A set of elevator IDs for all elevators in the system.
+     */
+    public Set<Integer> idSet();
+
+    /**
+     * Initiate a pickup request.
+     *
+     * @param pickupFloor Floor to be picked up on.
+     * @param destinationFloor Floor desired.
+     */
+    public void pickup(int pickupFloor, int destinationFloor);
+
+    /**
+     * Advance the state of the world by one time-step.
+     */
+    public void step();
+
 }
--- src/Triple.java
+++ src/Triple.java
@@ -1,5 +1,17 @@
 /**
- * Created by trevor on 2/4/15.
+ * Quick and dirty triple. Not meant to be good in any sort of way.
+ *
+ * @author Trevor Killeen (2015)
  */
-public class Triple {
+public class Triple<T> {
+
+    public T first;
+    public T second;
+    public T third;
+
+    public Triple(T first, T second, T third) {
+        this.first = first;
+        this.second = second;
+        this.third = third;
+    }
 }
